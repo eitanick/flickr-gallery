bf57d6146e762a2aa6fc1f6ecadcb89ccb0b3c2e
eitanick <37104452+eitanick@users.noreply.github.com>
2018-03-11 19:43:29 +0200

    delete and rotate functions

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -12,8 +12,12 @@ class Image extends React.Component {
   constructor(props) {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
+	this.onClick = this.onClick.bind(this);
+	this.rotate = this.rotate.bind(this);
     this.state = {
-      size: 200
+      size: 200,
+	  deleted:false,
+	  rotation: 0
     };
   }
 
@@ -27,6 +31,16 @@ class Image extends React.Component {
     });
   }
 
+   rotate(){
+    let newRotation = this.state.rotation + 90;
+    if(newRotation >= 360){
+      newRotation =- 360;
+    }
+    this.setState({
+      rotation: newRotation
+    })
+  }
+  
   componentDidMount() {
     this.calcImageSize();
   }
@@ -34,22 +48,46 @@ class Image extends React.Component {
   urlFromDto(dto) {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
-
+  
+  
+  onClick(){
+    this.setState({
+      deleted:true,
+	  size:0
+    });
+  }
+	
   render() {
-    return (
-      <div
-        className="image-root"
+    const isDeleted = this.state.deleted;
+	const { rotation } =  this.state;
+    let img = null;
+    if (isDeleted) {
+      img = <div className="image-root"
         style={{
-          backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
           width: this.state.size + 'px',
           height: this.state.size + 'px'
-        }}
+        }}></div>;
+    } else {
+      img = <div
+        
         >
         <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
+          <FontAwesome className="image-icon" name="sync-alt" title="rotate" onClick={this.rotate}/>
+          <FontAwesome className="image-icon" name="trash-alt" title="delete" onClick={this.onClick}/>
           <FontAwesome className="image-icon" name="expand" title="expand"/>
         </div>
+      </div>;
+    }
+
+    return (
+      <div className="image-root"
+        style={{
+          backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
+		  transform: `rotate(${rotation}deg)`,
+          width: this.state.size + 'px',
+          height: this.state.size + 'px'
+        }}>
+        {img}
       </div>
     );
   }
